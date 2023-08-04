ea2a7df26dea369cc836cb815d03fd5df389fb65
saranya <saranya@abc.com>
2023-08-04 15:44:52 +0000

    from testing local

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Welcome to java");
     System.out.println("From my git server");
+    System.out.println("From testing local");
   }
 }
